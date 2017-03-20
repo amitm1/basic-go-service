aea60a63aa6d73135060cfead79955e17df32482
Amit Mishra <amit.mishra@fox.com>
2017-03-20 12:21:25 -0700

    First Commit

---
+++ cmd/root_cmd.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/amitm1/basic-go-service/conf"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = cobra.Command{
+	Use: "example",
+	Run: run,
+}
+
+// RootCommand will setup and return the root command
+func RootCommand() *cobra.Command {
+	rootCmd.PersistentFlags().StringP("config", "c", "", "the config file to use")
+	rootCmd.Flags().IntP("port", "p", 0, "the port to use")
+
+	return &rootCmd
+}
+
+func run(cmd *cobra.Command, args []string) {
+	config, err := conf.LoadConfig(cmd)
+	if err != nil {
+		log.Fatal("Failed to load config: " + err.Error())
+	}
+
+	logger, err := conf.ConfigureLogging(&config.LogConfig)
+	if err != nil {
+		log.Fatal("Failed to configure logging: " + err.Error())
+	}
+
+	logger.Infof("Starting with config: %+v", config)
+}
